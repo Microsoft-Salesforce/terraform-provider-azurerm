3a2fde4a87a3ecb23b1e16e2e2036256b9b2f4b4
Priyanka Ranganath <pranganath@salesforce.com>
2020-09-22 23:04:10 +0530

    Removed azuread provider dependency wherever applicable for azuread diagnostic settings feature

--- azurerm/internal/location/validation.go
+++ azurerm/internal/location/validation.go
@@ -3,8 +3,7 @@ package location
 import (
 	"fmt"
 	"strings"
-
-	"github.com/terraform-providers/terraform-provider-azuread/azuread/helpers/validate"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-azurerm/azurerm/internal/features"
 )
 
@@ -18,7 +17,7 @@ var enhancedEnabled = features.EnhancedValidationEnabled()
 // fall back to the original approach
 func EnhancedValidate(i interface{}, k string) ([]string, []error) {
 	if !enhancedEnabled || supportedLocations == nil {
-		return validate.NoEmptyStrings(i, k)
+		return validation.StringIsNotEmpty(i, k)
 	}
 
 	return enhancedValidation(i, k)
